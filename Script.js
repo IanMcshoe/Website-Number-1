c5fd2abe947aac8b2b5d905cf3b9938df48f6379
Ian Forrester <ian.forrester46@gmail.com>
2022-08-03 23:57:27 -0500

    Added some JS

---
+++ Script.js
@@ -0,0 +1,18 @@
+let response = prompt("Social Security Number?");
+document.write(`Good day, ${response}, I See you've fallen into my SSN trap. I know my site looks like hot garbage, but for the life of me i can't seem to get any of my elements to line up with one another. It's causing me great pain...`)
+console.log(response);
+
+if (response == Ian) 
+    document.log("Please Fix Me.")
+    console.write("Please Fix Me.");
+
+    const btn = document.getElementById('btn');
+
+    btn.addEventListener('click', function onClick(event) {
+      const box = document.getElementById('box');
+    
+      box.style.backgroundColor = 'coral';
+    });
+
+
+

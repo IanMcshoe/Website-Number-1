a8851cee2191543388f670fdf90dc1e171126fef
Ian Forrester <ian.forrester46@gmail.com>
2022-08-04 14:59:27 -0500

    added and fixed JS

--- Script.js
+++ Script.js
@@ -1,20 +1,19 @@
-let response = prompt("Social Security Number?");
-document.write(`Good day, ${response}, I See you've fallen into my SSN trap. I know my site looks like hot garbage, but for the life of me i can't seem to get any of my elements to line up with one another. It's causing me great pain...`)
+let response = prompt("Favorite color");
+// document.write(`Good day, ${response}, I See you've fallen into my SSN trap. I know my site looks like hot garbage, but for the life of me i can't seem to get any of my elements to line up with one another. It's causing me great pain...`)
+// console.log(response);
+
+
+
+if (response == "purple"){
+    document.write("Mine too!");
+    console.log("Mine too!");
+}
+
+else {
+    let response = prompt("Wrong. Try again.");
+document.write("Clearly you are a person of poor taste.")
 console.log(response);
 
-const = ian
-
-if (response == ian) 
-    document.log("Please Fix Me.")
-    console.write("Please Fix Me.");
-
-    const btn = document.getElementById('btn');
-
-    btn.addEventListener('click', function onClick(event) {
-      const box = document.getElementById('box');
-    
-      box.style.backgroundColor = 'coral';
-    });
-
+}
 
 

de4cde1158fbe618b0901e4bee231f51aac16e90
Ian Forrester <ian.forrester46@gmail.com>
2022-08-04 00:26:56 -0500

    fixed JS

--- Script.js
+++ Script.js
@@ -2,7 +2,9 @@ let response = prompt("Social Security Number?");
 document.write(`Good day, ${response}, I See you've fallen into my SSN trap. I know my site looks like hot garbage, but for the life of me i can't seem to get any of my elements to line up with one another. It's causing me great pain...`)
 console.log(response);
 
-if (response == Ian) 
+const = ian
+
+if (response == ian) 
     document.log("Please Fix Me.")
     console.write("Please Fix Me.");
 

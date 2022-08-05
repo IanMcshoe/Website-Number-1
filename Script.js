41438e5d60de99643247a8bab1ba09dcd4fecaa6
Ian Forrester <ian.forrester46@gmail.com>
2022-08-05 16:28:17 -0500

    added validation

--- Script.js
+++ Script.js
@@ -9,6 +9,7 @@ while (parsed > 5 && parsed < 5 && typeof parsed == "number") {
 };
 
 for (let i = 0; i < parsed; i++){
+    alert("Thank You!")
     document.write("<img src='https://cdn-icons-png.flaticon.com/512/1828/1828961.png'/>")
 };
 // if (response == "purple"){

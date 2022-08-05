e3ded65a95688b55b579460a47d92b1faf76993c
Ian Forrester <ian.forrester46@gmail.com>
2022-08-05 17:07:58 -0500

    added 2 JS elments.

--- Script.js
+++ Script.js
@@ -5,23 +5,32 @@ while (parsed > 5 && parsed < 5 && typeof parsed == "number") {
     alert("1 through 5 please!");
     response = prompt("What would you rate this site 1-5?");
     parsed = parseInt(response);
+    
 
 };
+ let somethingElse = prompt("What's your favorite color?");
+
+if (somethingElse == "purple"){
+    document.write("Mine too!");
+    console.log("Mine too!");
+}
+
+else {
+    let somethingElse = prompt("Wrong. Try again. Hint. Red + Blue");
+document.write("Cleary you are a person of poor taste.")
+console.log(somethingElse);
+
+}
+
+   
+
+    
+
+
+alert("Thank You!");
 
 for (let i = 0; i < parsed; i++){
-    alert("Thank You!")
-    document.write("<img src='https://cdn-icons-png.flaticon.com/512/1828/1828961.png'/>")
+    document.write("<img src='https://cdn-icons-png.flaticon.com/512/1828/1828961.png'/>");
 };
-// if (response == "purple"){
-//     document.write("Mine too!");
-//     console.log("Mine too!");
-// }
-
-// else {
-//     let response = prompt("Wrong. Try again.");
-// document.write("Cleary you are a person of poor taste.")
-// console.log(response);
-
-// }
 
 
